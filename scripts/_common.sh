00e92cebf64e91b2d1526eea814579f3ebc81d44
Augustin Trancart <augustin.trancart@oslandia.com>
2019-12-02 21:01:24 +0100

    Fix install script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pkg_dependencies="build-essential cmake libssl-dev libcurl4-openssl-dev libxml2-dev libxslt-dev imagemagick ghostscript curl libmagickwand-dev git libpq-dev redis-server nodejs"
+pkg_dependencies="build-essential cmake libssl-dev libcurl4-openssl-dev libxml2-dev libxslt-dev imagemagick ghostscript curl libmagickwand-dev git libpq-dev redis-server nodejs postgresql bison "
+ruby_build_dependencies="bison libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libyaml-dev pkg-config sqlite3 zlib1g-dev libgmp-dev libreadline-dev libssl-dev"
